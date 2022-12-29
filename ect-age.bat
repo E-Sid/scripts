56447de3b474530a85bf5e67737e0e3c08f8405c
Sid <62950792+E-Sid@users.noreply.github.com>
2022-12-29 22:43:53 +0000

    Add files via upload

--- ect-age.bat
+++ ect-age.bat
@@ -2,4 +2,6 @@ echo off
 echo This is ECT Age-based charge calculator
 SET/P A="Please, enter the age of the patient in years"
 SET /A "D=%A%*5
+SET /A "H=%A%*2.5"
 echo According to age-based dosing the estimated dose is %D% mC
+echo According to half age-based dosing the estimated dose is %H% mC
