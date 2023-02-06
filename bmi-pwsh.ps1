cebe656cebcd2d4ee7782aa19e8a5701e545e7d4
Sid <62950792+E-Sid@users.noreply.github.com>
2023-02-06 16:29:14 +0000

    Add files via upload

---
+++ bmi-pwsh.ps1
@@ -0,0 +1,4 @@
+﻿[decimal]$W = Read-Host -Prompt 'Please, enter weight in Kilograms'
+[decimal]$H = Read-Host -Prompt 'Please, enter height in meters'
+[decimal]$BMI = $W/($H*$H)
+write-host 'The BMI is' $BMI
