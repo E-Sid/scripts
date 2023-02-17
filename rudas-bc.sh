b4e532dc40191c40eb3cb1135f238374e48931a3
Sid <62950792+E-Sid@users.noreply.github.com>
2023-02-17 22:35:21 +0000

    Add files via upload

---
+++ rudas-bc.sh
@@ -0,0 +1,60 @@
+#/!/bin/bash
+
+echo "This is Rowland Universal Dementia Assessment Scale (RUDAS) score calculator"
+echo "============================================================================"
+echo "Please, type score for visuospatial orientation [0-5]"
+while :; do
+    read VSO
+    if [[ $VSO -ge 0 && $VSO -le 5 ]]; then
+	break
+    else
+	echo "Value is out of range. Please, enter a valid number"
+    fi
+done
+echo "Please, type score for praxis [0-2]"
+while :; do
+    read P
+    if [[ $P -ge 0 && $P -le 2 ]]; then
+	break
+    else
+	echo "Value is out of range. Please, enter a valid number"
+    fi
+done
+echo "Please, type score for visuoconstructional drawing [0-3]"
+while :; do
+    read VCD
+    if [[ $VCD -ge 0 && $VCD -le 3 ]]; then
+	break
+    else
+	echo "Value is out of range. Please, enter a valid number"
+    fi
+done
+echo "Please, type score for judgement [0-4]"
+while :; do
+    read J
+    if [[ $J -ge 0 && $J -le 4 ]]; then
+	break
+    else
+	echo "Value is out of range. Please, enter a valid number"
+    fi
+done
+echo "Please, type score for memory recall [0-8]"
+while :; do
+    read M
+    if [[ $M -ge 0 && $M -le 8 ]]; then
+	break
+    else
+	echo "Value is out of range. Please, enter a valid number"
+    fi
+done
+echo "Please, type score for language [0-8]"
+while :; do
+    read L
+    if [[ $L -ge 0 && $L -le 8 ]]; then
+	break
+    else
+	echo "Value is out of range. Please, enter a valid number"
+    fi
+done
+rudas=$(echo $VSO + $P + $VCD + $J + $M + $L | bc)
+echo "The total RUDAS score is" $rudas"/30"
