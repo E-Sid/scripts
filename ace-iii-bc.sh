05ad5489550dc3fd130d7b2662b8cfc77ce5f50b
Sid <62950792+E-Sid@users.noreply.github.com>
2023-03-10 21:51:18 +0000

    Add files via upload

--- ace-iii-bc.sh
+++ ace-iii-bc.sh
@@ -297,66 +297,91 @@ visuospatial=$(echo $V1 + $V2 + $V3 + $V4 + $V5 | bc)
 echo "The Visuospatial subset score is" $visuospatial "/16"
 echo "According to Matias-Guiu, et al(2018) doi:10.1017/S104161021700268X"
 if [[ $ace -ge 0 && $ace -le 7 ]]; then
-    echo "MMSE score is 0/30"
+    echo "MMSE score is 0/30" ; mmse="0"
     elif [[ $ace -ge 8 && $ace -le 10 ]]; then
-    echo "MMSE score is 1/30"
+    echo "MMSE score is 1/30" ; mmse="1"
     elif [[  $ace -ge 11 && $ace -le 12 ]]; then
-    echo "MMSE score is 2/30"
+    echo "MMSE score is 2/30" ; mmse="2"
     elif [[  $ace -ge 13 && $ace -le 14 ]]; then
-    echo "MMSE score is 3/30"
+    echo "MMSE score is 3/30" ; mmse="3"
     elif [[  $ace -ge 15 && $ace -le 16 ]]; then
-    echo "MMSE score is 4/30"
+    echo "MMSE score is 4/30" ; mmse="4"
     elif [[  $ace -ge 17 && $ace -le 17 ]]; then
-    echo "MMSE score is 5/30"
+    echo "MMSE score is 5/30" ; mmse="5"
     elif [[  $ace -ge 18 && $ace -le 19 ]]; then
-    echo "MMSE score is 6/30"
+    echo "MMSE score is 6/30" ; mmse="6"
     elif [[  $ace -ge 20 && $ace -le 21 ]]; then
-    echo "MMSE score is 7/30"
+    echo "MMSE score is 7/30" ; mmse="7"
     elif [[  $ace -ge 22 && $ace -le 22 ]]; then
-    echo "MMSE score is 8/30"
+    echo "MMSE score is 8/30" ; mmse="8"
     elif [[  $ace -ge 23 && $ace -le 24 ]]; then
-    echo "MMSE score is 9/30"
+    echo "MMSE score is 9/30" ; mmse="9"
     elif [[  $ace -ge 25 && $ace -le 26 ]]; then
-    echo "MMSE score is 10/30"
+    echo "MMSE score is 10/30" ; mmse="10"
     elif [[  $ace -ge 27 && $ace -le 28 ]]; then
-    echo "MMSE score is 11/30"
+    echo "MMSE score is 11/30" ; mmse="11"
     elif [[  $ace -ge 29 && $ace -le 31 ]]; then
-    echo "MMSE score is 12/30"
+    echo "MMSE score is 12/30" ; mmse="12"
     elif [[  $ace -ge 32 && $ace -le 33 ]]; then
-    echo "MMSE score is 13/30"
+    echo "MMSE score is 13/30" ; mmse="13"
     elif [[  $ace -ge 34 && $ace -le 36 ]]; then
-    echo "MMSE score is 14/30"
+    echo "MMSE score is 14/30" ; mmse="14"
     elif [[  $ace -ge 37 && $ace -le 38 ]]; then
-    echo "MMSE score is 15/30"
+    echo "MMSE score is 15/30" ; mmse="15"
     elif [[  $ace -ge 39 && $ace -le 41 ]]; then
-    echo "MMSE score is 16/30"
+    echo "MMSE score is 16/30" ; mmse="16"
     elif [[  $ace -ge 42 && $ace -le 44 ]]; then
-    echo "MMSE score is 17/30"
+    echo "MMSE score is 17/30" ; mmse="17"
     elif [[  $ace -ge 45 && $ace -le 48 ]]; then
-    echo "MMSE score is 18/30"
+    echo "MMSE score is 18/30" ; mmse="18"
     elif [[  $ace -ge 49 && $ace -le 51 ]]; then
-    echo "MMSE score is 19/30"
+    echo "MMSE score is 19/30" ; mmse="19"
     elif [[  $ace -ge 52 && $ace -le 55 ]]; then
-    echo "MMSE score is 20/30"
+    echo "MMSE score is 20/30" ; mmse="20"
     elif [[  $ace -ge 56 && $ace -le 59 ]]; then
-    echo "MMSE score is 21/30"
+    echo "MMSE score is 21/30" ; mmse="21"
     elif [[  $ace -ge 60 && $ace -le 63 ]]; then
-    echo "MMSE score is 22/30"
+    echo "MMSE score is 22/30" ; mmse="22"
     elif [[  $ace -ge 64 && $ace -le 68 ]]; then
-    echo "MMSE score is 23/30"
+    echo "MMSE score is 23/30" ; mmse="23"
     elif [[  $ace -ge 69 && $ace -le 73 ]]; then
-    echo "MMSE score is 24/30"
+    echo "MMSE score is 24/30" ; mmse="24"
     elif [[  $ace -ge 74 && $ace -le 78 ]]; then
-    echo "MMSE score is 25/30"
+    echo "MMSE score is 25/30" ; mmse="25"
     elif [[  $ace -ge 79 && $ace -le 83 ]]; then
-    echo "MMSE score is 26/30"
+    echo "MMSE score is 26/30" ; mmse="26"
     elif [[  $ace -ge 84 && $ace -le 88 ]]; then
-    echo "MMSE score is 27/30"
+    echo "MMSE score is 27/30" ; mmse="27"
     elif [[  $ace -ge 89 && $ace -le 93 ]]; then
-    echo "MMSE score is 28/30"
+    echo "MMSE score is 28/30" ; mmse="28"
     elif [[  $ace -ge 94 && $ace -le 98 ]]; then
-    echo "MMSE score is 29/30"
+    echo "MMSE score is 29/30" ; mmse="29"
     elif [[  $ace -ge 99 && $ace -le 100 ]]; then
-    echo "MMSE score is 30/30"
+    echo "MMSE score is 30/30" ; mmse="30"
 fi
-    
+now="$(date)"
+unix="$(date +%s)"
+echo "Do you want to save the results [y/n]?"
+while true; do
+read ans
+case $ans in
+    [Yy] )  
+	echo "Unix Epoch is "$unix "
+Date is "$now "
+The total ACE-III score is" $ace"/100
+The total M-ACE score is" $mace"/30 
+The Attention subset score is" $attention"/18
+The Memory subset score is" $memory"/26
+The Fluency subset score is" $fluency"/14
+The Language subset score is" $language"/26
+The Visuospatial abilities subset score is" $visuospatial"/16
+According to Matias-Guiu, et al(2018) doi:10.1017/S104161021700268X
+The estimated MMSE score is" $mmse"30" > ace-iii-output.txt;
+	       echo "The ACE-III score is saved to ace-iii-output.txt"
+	       break;;
+	[Nn] ) echo "Not saved";
+	       exit;;
+	   * ) echo "Invalid Response"
+esac
+done
+
