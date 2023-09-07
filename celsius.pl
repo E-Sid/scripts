e0000ecc955cbd850198451836db29073e6eeb12
Sid <62950792+E-Sid@users.noreply.github.com>
2023-09-07 21:45:28 +0100

    Add files via upload

--- celsius.pl
+++ celsius.pl
@@ -2,6 +2,14 @@
 use strict;
 use warnings;
 
+eval 'exec /usr/bin/perl -S $0 ${1+"$@"}'
+    if my $running_under_some_shell;
+			# this emulates #! processing on NIH machines.
+			# (remove #! line above if indigestible)
+
+eval '$'.$1.'$2;' while $ARGV[0] =~ /^([A-Za-z_0-9]+=)(.*)/ && shift;
+			# process any FOO=bar switches
+
 # This equation is based on Programming in Perl book
 
 print "This is Temperature Scale: Fahrenheit to Celsius converter\n";
@@ -17,3 +25,28 @@ chomp($temp);
     my $celsius = ($temp-= 32)*=5/9;
 print "The temperature in Celsius is ". $celsius . " C";
     print "\n";
+while (1) {
+print "Do you want to save the output to file (F2C.txt) [yes or no]? ";
+my $ans = lc(<STDIN>);
+chomp($ans);
+if ($ans eq 'yes')  {	   
+    my $output = 'F2C.txt';
+open(FH,'>', $output) or die $!;
+print FH 
+my $str = <<END;
+The temperature in Celsius is $celsius C
+END
+# print FH $str;
+close(FH);
+    print "written to F2C.txt\n";
+    last;
+}	
+elsif ($ans eq 'no') {
+    print "not saved\n";
+    last;
+	}
+else {
+print "out of range, please answer [yes or no] ";
+ }
+}
+
